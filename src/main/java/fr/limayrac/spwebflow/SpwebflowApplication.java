5a2d3afb878ccebf2295c929811d687a3c61f735
Reyem501 <59642674+Reyem501@users.noreply.github.com>
2024-01-07 17:27:13 +0100

    initial commit avec une app qui marche (après 2 autres qui marchées pas...)

---
+++ src/main/java/fr/limayrac/spwebflow/SpwebflowApplication.java
@@ -0,0 +1,13 @@
+package fr.limayrac.spwebflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpwebflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpwebflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/limayrac/spwebflow/SpwebflowApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.limayrac.spwebflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpwebflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
